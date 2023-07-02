00fae6e6b8a7c6465b6242d42ae5c258d4f7e182
Ben Tsui <ben@bentsui.dev>
2023-07-02 15:20:00 -0600

    tet change

--- src/App.tsx
+++ src/App.tsx
@@ -16,13 +16,13 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>This is Ben testing in production</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          This is a work in progress
         </p>
       </div>
       <p className="read-the-docs">
