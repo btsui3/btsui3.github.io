ab768e3a57695cfc32a664e566f4810f8f860e60
Ben Tsui <ben@bentsui.dev>
2023-07-02 23:37:23 -0600

    three js

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,83 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import * as THREE from 'three'
 
 function App() {
   const [count, setCount] = useState(0)
+
+  // Setup
+
+  const scene = new THREE.Scene();
+
+  const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+  const renderer = new THREE.WebGLRenderer({
+    canvas: document.querySelector('#bg') || undefined,
+  });
+
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  camera.position.setZ(30);
+  camera.position.setX(-3);
+
+  renderer.render(scene, camera);
+
+  // Torus
+
+  const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
+  const material = new THREE.MeshStandardMaterial({ color: 0xff6347 });
+  const torus = new THREE.Mesh(geometry, material);
+
+  scene.add(torus);
+
+  // Lights
+
+  const pointLight = new THREE.PointLight(0xffffff);
+  pointLight.position.set(5, 5, 5);
+
+  const ambientLight = new THREE.AmbientLight(0xffffff);
+  scene.add(pointLight, ambientLight);
+
+
+
+  // Avatar
+
+  const benTexture = new THREE.TextureLoader().load('./assets/ben.png');
+
+  const ben = new THREE.Mesh(new THREE.BoxGeometry(3, 3, 3), new THREE.MeshBasicMaterial({ map: benTexture }));
+  ben.position.z = -5;
+  ben.position.x = 2;
+  scene.add(ben);
+  function moveCamera() {
+
+    ben.rotation.y += 0.01;
+    ben.rotation.z += 0.01;
+
+    // camera.position.z = t * -0.01;
+    // camera.position.x = t * -0.0002;
+    // camera.rotation.y = t * -0.0002;
+  }
+
+  document.body.onscroll = moveCamera;
+  moveCamera();
+  // Animation Loop
+
+  function animate() {
+    requestAnimationFrame(animate);
+
+    torus.rotation.x += 0.01;
+    torus.rotation.y += 0.005;
+    torus.rotation.z += 0.01;
+
+
+    // controls.update();
+
+    renderer.render(scene, camera);
+  }
+
+  animate();
+
 
   return (
     <>
