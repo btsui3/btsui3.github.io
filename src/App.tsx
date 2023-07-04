f2577c752bf6142d977c71d496ef50ce9295b8e6
Ben Tsui <ben@bentsui.dev>
2023-07-04 00:34:06 -0600

    add images

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import * as THREE from 'three'
-
+import './App.css';
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 function App() {
-  const [count, setCount] = useState(0)
-
-  // Setup
 
   const scene = new THREE.Scene();
-
   const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
   const renderer = new THREE.WebGLRenderer({
@@ -40,70 +33,70 @@ function App() {
   const ambientLight = new THREE.AmbientLight(0xffffff);
   scene.add(pointLight, ambientLight);
 
+  const controls = new OrbitControls(camera, renderer.domElement);
 
+  function addStar() {
+    const geometry = new THREE.SphereGeometry(0.25, 24, 24);
+    const material = new THREE.MeshStandardMaterial({ color: 0xffffff });
+    const star = new THREE.Mesh(geometry, material);
+
+    const [x, y, z] = Array(3)
+      .fill()
+      .map(() => THREE.MathUtils.randFloatSpread(100));
+
+    star.position.set(x, y, z);
+    scene.add(star);
+  }
+
+  Array(200).fill().forEach(addStar);
 
   // Avatar
-
-  const benTexture = new THREE.TextureLoader().load('./assets/ben.png');
-
+  const benTexture = new THREE.TextureLoader().load('src/assets/Ben.png');
   const ben = new THREE.Mesh(new THREE.BoxGeometry(3, 3, 3), new THREE.MeshBasicMaterial({ map: benTexture }));
+
+  scene.add(ben);
+
+  const moonTexture = new THREE.TextureLoader().load('src/assets/moon.jpg');
+  const normalTexture = new THREE.TextureLoader().load('src/assets/normal.jpg');
+
+  const moon = new THREE.Mesh(
+    new THREE.SphereGeometry(3, 32, 32),
+    new THREE.MeshStandardMaterial({
+      map: moonTexture,
+      normalMap: normalTexture,
+    })
+  );
+
+  scene.add(moon);
+
+  moon.position.z = 30;
+  moon.position.setX(-10);
+
   ben.position.z = -5;
   ben.position.x = 2;
-  scene.add(ben);
-  function moveCamera() {
 
+  function moveCamera() {
     ben.rotation.y += 0.01;
     ben.rotation.z += 0.01;
-
-    // camera.position.z = t * -0.01;
-    // camera.position.x = t * -0.0002;
-    // camera.rotation.y = t * -0.0002;
   }
 
   document.body.onscroll = moveCamera;
   moveCamera();
-  // Animation Loop
 
   function animate() {
     requestAnimationFrame(animate);
-
     torus.rotation.x += 0.01;
     torus.rotation.y += 0.005;
     torus.rotation.z += 0.01;
 
-
-    // controls.update();
+    controls.update();
 
     renderer.render(scene, camera);
   }
 
   animate();
 
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>This is Ben testing in production</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          This is a work in progress
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  return
 }
 
 export default App
