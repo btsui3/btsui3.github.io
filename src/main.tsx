bbc8ff6934b972f386343c44d5db93bc997a0141
Ben Tsui <ben@bentsui.dev>
2023-07-30 15:45:14 -0600

    add router and error page

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,28 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import Root from "./routes/root";
+import ErrorPage from "./error-page";
 
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/btsui3.github.io/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+  },
+]);
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
